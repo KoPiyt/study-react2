ec60e20898fdf48189cefa199a0b74f296e8d261
KoPiyt <108018496+KoPiyt@users.noreply.github.com>
2023-12-28 17:26:37 +0900

    第4回まで見ました。propsとchiildrenを理解

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
+
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Footer(props) {
+export function Footer(props) {
   return (
         <div className={styles.grid}>
           <a
